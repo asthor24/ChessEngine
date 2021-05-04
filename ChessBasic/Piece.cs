5550fbd5245bc49ab44a774a139ac254aeb8c68a
Asthor Bjornsson <asthor02@gmail.com>
2021-05-04 02:06:38 +0200

    changes piece an position to structs

--- ChessBasic/Piece.cs
+++ ChessBasic/Piece.cs
@@ -1,5 +1,5 @@
 namespace ChessEngine.ChessBasic {
-    public class Piece : IPiece {
+    public struct Piece : IPiece {
         public PieceType Type { get; set; }
         public Color Color { get; set; }
         public ICell Placement { get; set; }
--- ChessBasic/Position.cs
+++ ChessBasic/Position.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 
 namespace ChessEngine.ChessBasic {
-    public class Position : IPosition {
+    public struct Position : IPosition {
         public IEnumerable<IPiece> Pieces { get; set; }
     }
 }
